d818baadf7e5cc90691c24facd1bccde9a3b179c
David-Vandensteen <dvandensteen@gmail.com>
2026-05-02 12:52:53 +0200

    Enhance LedBlink driver with infinite mode and improved cycle handling
    Refactor the LedBlink class to support continuous blinking using a new infinite constant. This update renames "iteration" to "cycle" for clarity, ensures the cycle count only decrements upon completion of a full off-on-off sequence, and cleans up constructors using initializer lists.

--- src/driver/led_blink.cpp
+++ src/driver/led_blink.cpp
@@ -1,34 +1,28 @@
 #include <Arduino.h>
 #include "led_blink.h"
 
-LedBlink::LedBlink() : _pin(0) {
-  _state.current = LOW;
-  _state.interval = 0;
-  _state.iteration = 0;
-  _state.lastChange = 0;
-}
+LedBlink::LedBlink() : _pin(0), _state{LOW, 100, LED_BLINK_INFINITE, 0} {}
 
-LedBlink::LedBlink(uint8_t pin) : _pin(pin) {
-  LedBlink::LedBlink();
+LedBlink::LedBlink(uint8_t pin) : _pin(pin), _state{LOW, 100, LED_BLINK_INFINITE, 0} {
   pinMode(_pin, OUTPUT);
 }
 
-LedBlink::LedBlink(uint8_t pin, unsigned long interval, uint8_t iteration) : _pin(pin) {
+LedBlink::LedBlink(uint8_t pin, unsigned long interval) : _pin(pin), _state{LOW, interval, LED_BLINK_INFINITE, 0} {
   pinMode(_pin, OUTPUT);
-  _state.current = LOW;
-  _state.interval = interval;
-  _state.iteration = iteration;
-  _state.lastChange = 0;
+}
+
+LedBlink::LedBlink(uint8_t pin, unsigned long interval, uint8_t cycle) : _pin(pin), _state{LOW, interval, cycle, 0} {
+  pinMode(_pin, OUTPUT);
 }
 
 void LedBlink::update() {
-  if (_state.iteration > 0) {
+  if (_state.cycle > 0 || _state.cycle == 255) {
     unsigned long now = millis();
     if (now - _state.lastChange >= _state.interval) {
       _state.current = !_state.current;
       digitalWrite(_pin, _state.current);
       _state.lastChange = now;
-      _state.iteration--;
+      if (_state.current == LOW && _state.cycle != 255) _state.cycle--;
     }
   }
 }
--- src/driver/led_blink.h
+++ src/driver/led_blink.h
@@ -2,19 +2,24 @@
 #define LED_BLINK_H
 #include <Arduino.h>
 
+#define LED_BLINK_INFINITE 255
+
 class LedBlink {
 public:
   explicit LedBlink();
   explicit LedBlink(uint8_t pin);
-  explicit LedBlink(uint8_t pin, unsigned long interval, uint8_t iteration);
+  explicit LedBlink(uint8_t pin, unsigned long interval);
+  explicit LedBlink(uint8_t pin, unsigned long interval, uint8_t cycle = LED_BLINK_INFINITE);
   void update();
 
 private:
   struct State {
     bool current = LOW;
-    uint8_t iteration = 0;
-    unsigned long interval = 0;
+    uint8_t cycle = LED_BLINK_INFINITE;
+    unsigned long interval = 100;
     unsigned long lastChange = 0;
+    State(bool current, unsigned long interval, uint8_t cycle, unsigned long lastChange)
+      : current(current), cycle(cycle), interval(interval), lastChange(lastChange) {}
   };
   uint8_t _pin;
   State _state;
