b81542998caeb0da35b809c2fdf578fa53c09daf
David-Vandensteen <dvandensteen@gmail.com>
2026-05-08 11:17:31 +0200

    Remove unused LedProtocol reference from driver sketch
    The `LedProtocol` include and static variable have been removed from the main sketch. This cleanup follows recent refactors that centralized hardware protocol management and dependency injection within the `Driver` class, making these references redundant in the entry point.

--- src/driver/driver.ino
+++ src/driver/driver.ino
@@ -7,11 +7,9 @@
 #include <Arduino.h>
 #include "driver.h"
 #include "setting.h"
-#include "led_protocol.h"
 #include "trackers.h"
 
 static Setting setting;
-static LedProtocol *ledProtocol;
 static Trackers *trackers;
 
 void setup() {
